1dd69159335db17b75698b9ebc26b36ab2ce9a4a
Alexander Hupp <ahupp@ashnet.de>
2021-10-05 02:31:12 +0200

    Corrected Pointers for Odyssey & Horizon Update 7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ashnet GmbH")]
 [assembly: AssemblyProduct("EliteDangerousTelemetryProvider")]
-[assembly: AssemblyCopyright("Copyright © ashnet GmbH 2020")]
+[assembly: AssemblyCopyright("Copyright © ashnet GmbH 2021")]
 [assembly: AssemblyTrademark("SimFeedback")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.6.0")]
-[assembly: AssemblyFileVersion("0.0.6.0")]
+[assembly: AssemblyVersion("0.0.8.0")]
+[assembly: AssemblyFileVersion("0.0.8.0")]
